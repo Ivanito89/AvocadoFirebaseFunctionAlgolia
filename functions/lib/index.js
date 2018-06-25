d238b094b2bc926fc90492656d02289779d121fb
Ivan Hjelmeland <ivan.hjelmeland@shortcut.no>
2018-06-25 10:05:19 +0200

    added update function and fixed issues related to "ObjectID" and "ObjectId"

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -7,18 +7,19 @@ const env = functions.config();
 const algoliasearch = require("algoliasearch");
 const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
 const index = client.initIndex('food_search');
-exports.indexFood = functions.firestore
-    .document('food/{foodId}')
-    .onCreate((snap, context) => {
+exports.indexFood = functions.firestore.document('food/{foodId}').onCreate((snap, context) => {
     const data = snap.data();
-    const objectId = snap.id;
-    return index.addObject(Object.assign({ objectId }, data));
+    const objectID = snap.id;
+    return index.addObject(Object.assign({ objectID }, data));
 });
-exports.unindexFood = functions.firestore
-    .document('food/{foodID}')
-    .onDelete((snap, context) => {
-    const objectId = snap.id;
+exports.updateFood = functions.firestore.document('food/{foodId}').onUpdate((snap, context) => {
+    const data = snap.after.data();
+    const objectID = snap.after.id;
+    return index.addObject(Object.assign({ objectID }, data));
+});
+exports.unindexFood = functions.firestore.document('food/{foodID}').onDelete((snap, context) => {
+    const objectID = snap.id;
     // Delete an ID from the index
-    return index.deleteObject(objectId);
+    return index.deleteObject(objectID);
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,23 +8,29 @@ import * as algoliasearch from 'algoliasearch';
 const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
 const index = client.initIndex('food_search');
 
-exports.indexFood = functions.firestore
-    .document('food/{foodId}')
-    .onCreate((snap, context) => {
+exports.indexFood = functions.firestore.document('food/{foodId}').onCreate((snap, context) => {
         const data = snap.data();
-        const objectId = snap.id;
+        const objectID = snap.id;
 
         return index.addObject({
-            objectId,
+            objectID,
             ...data
         });
 });
 
-exports.unindexFood = functions.firestore
-    .document('food/{foodID}')
-    .onDelete((snap, context) => {
-        const objectId = snap.id;
+exports.updateFood = functions.firestore.document('food/{foodId}').onUpdate((snap, context) => {
+    const data = snap.after.data();
+    const objectID = snap.after.id;
+
+    return index.addObject({
+        objectID,
+        ...data
+    });
+});
+
+exports.unindexFood = functions.firestore.document('food/{foodID}') .onDelete((snap, context) => {
+        const objectID = snap.id;
 
         // Delete an ID from the index
-        return index.deleteObject(objectId);
+        return index.deleteObject(objectID);
 });
